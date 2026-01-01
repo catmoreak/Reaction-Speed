8077a8d2611b002e557f652b4d58b7b916adc66b
catmoreak <72437057+catmoreak@users.noreply.github.com>
2026-01-01 13:37:20 +0530

    [feat] leaderbord

--- my-app/src/app/leaderboard/page.tsx
+++ my-app/src/app/leaderboard/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Leaderboard = () => {
   return (
-    <div>
-      <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-900 via-black to-blue-900">
-        <h1 className="text-4xl font-bold text-white">Leaderboard Page - Coming Soon</h1>
-      </div>
+    <div className="min-h-screen flex items-center justify-center cyber-grid font-mono text-white">
+     <div className="relative z-10 text-center max-w-3xl mx-auto p-6 bg-black/80 backdrop-blur-sm rounded-lg border-2 border-green-500/50 shadow-lg shadow-blue-500/10">
+        <h1 className="text-4xl font-bold text-white mb-4 uppercase" style={{ textShadow: '0 0 10px #00f' }}>
+          Leaderboard coming soonnnn
+        </h1>
     </div>
-  )
-}
+    </div>
+  );
+
+};
 
 export default Leaderboard;
